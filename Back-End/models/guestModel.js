5a0143f9bb395086fd4a7941f4799eb19ec7cb7d
Amit Kumar <amitkumar655921@gmail.com>
2024-02-20 23:18:11 +0530

    Update guestModel.js

--- Back-End/models/guestModel.js
+++ Back-End/models/guestModel.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const guestSchema = mongoose.Schema({
   name: { type: String, require: true },
   email: { type: String, require: true },
-  password: { type: String, require: true },
   phoneNo: { type: String, require: true },
   englishFluency: { type: Number, require: true },
   techStack: { type: Array, require: true },
